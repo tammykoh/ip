b2a5cdebeb5ac97dde7f494ec246f2d023f90996
tammykoh <tammykohrw@gmail.com>
2020-08-31 22:06:34 +0800

    Duke greets the user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,7 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo + "Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
