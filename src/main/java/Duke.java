28e83c47cc23baac6b0b869963a8f30f00b373c0
tammykoh <tammykohrw@gmail.com>
2020-09-02 00:43:01 +0800

    Include function where Duke echos the commands entered by user, and exits when the user types bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+
+        displayWelcomeMessage();
+        while (true) {
+            String userCommand = getUserInput();
+            echoUserCommand(userCommand);
+        }
+    }
+
+    private static void displayWelcomeMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(logo + "Hello! I'm Duke\n" + "What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(logo + "Hey there! I'm Duke\n" + "How can I be of service?");
+    }
+
+    private static void displayGoodbyeMessage() {
+        System.out.println("Goodbye. Hope to see you again soon! :)");
+    }
+
+    private static String getUserInput() {
+        Scanner sc = new Scanner(System.in);
+        String userCommand = sc.nextLine();
+        if (userCommand.equals("bye")) {
+            displayGoodbyeMessage();
+            System.exit(0);
+        }
+
+        return userCommand;
+    }
+
+    private static void echoUserCommand(String userCommand) {
+        System.out.println(userCommand);
     }
 }
