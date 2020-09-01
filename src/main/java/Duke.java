b6805bd261dc689e574df857ae0620470460f98c
tammykoh <tammykohrw@gmail.com>
2020-09-02 03:12:57 +0800

    Duke is now able to add items to a list, and display them when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,28 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
 
+     // Maximum number of tasks that can be held
+    private static final int MAX_TASK = 100;
+
+    // List of all tasks
+    private static String[] allTasks;
+
+    // Total number of tasks in the list
+    private static int count;
+
+    public static void main(String[] args) {
+        initTaskList();
         displayWelcomeMessage();
         while (true) {
             String userCommand = getUserInput();
-            echoUserCommand(userCommand);
+            executeUserCommand(userCommand);
         }
+    }
+
+    private static void initTaskList() {
+        allTasks = new String[MAX_TASK];
+        count = 0;
     }
 
     private static void displayWelcomeMessage() {
@@ -16,7 +31,7 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(logo + "Hey there! I'm Duke\n" + "How can I be of service?");
+        System.out.println(logo + "Hey there! I'm Duke.\n" + "How may I be of service?");
     }
 
     private static void displayGoodbyeMessage() {
@@ -26,15 +41,40 @@ public class Duke {
     private static String getUserInput() {
         Scanner sc = new Scanner(System.in);
         String userCommand = sc.nextLine();
-        if (userCommand.equals("bye")) {
-            displayGoodbyeMessage();
-            System.exit(0);
-        }
-
         return userCommand;
     }
 
+    private static void executeUserCommand(String userCommand) {
+        switch(userCommand) {
+        case "list":
+            displayList();
+            break;
+        case "bye":
+            exitProgram();
+            break;
+        default:
+            echoUserCommand(userCommand);
+            break;
+        }
+    }
+
     private static void echoUserCommand(String userCommand) {
-        System.out.println(userCommand);
+        allTasks[count] = userCommand;
+        System.out.println("added: " + userCommand);
+        count++;
+    }
+
+    private static void displayList() {
+        int taskNum = 1;
+        String[] taskList = allTasks;
+        for (int i=0; i<count; i++) {
+            System.out.println(taskNum + ". " + taskList[i]);
+            taskNum++;
+        }
+    }
+
+    private static void exitProgram() {
+        displayGoodbyeMessage();
+        System.exit(0);
     }
 }
